e11ef6dadc1aeffd9bc88cbf9f37f62312c4e9d4
weihua <weihua@springboot>
2019-08-19 16:05:03 +0800

    Consul

---
+++ src/main/java/com/consul/producer/SpringCloudConsulProducerApplication.java
@@ -0,0 +1,20 @@
+package com.consul.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringCloudConsulProducerApplication {
+
+	@RequestMapping("/say_hello")
+	public String SayHello() {
+		return "Hello, Spring Cloud Consul.";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConsulProducerApplication.class, args);
+	}
+
+}
